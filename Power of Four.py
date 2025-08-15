dc89440592d60615ce1d7909f4885d2615d90e56
mw <76392789+maximilianwynne@users.noreply.github.com>
2025-08-15 11:52:12 +0200

    Update Power of Four.py

--- Power of Four.py
+++ Power of Four.py
@@ -1,4 +1,4 @@
-## Given an integer n, return true if it is a power of four. Otherwise, return false. An integer n is a power of four, if there exists an integer x such that n == 4x.
+// Given an integer n, return true if it is a power of four. Otherwise, return false. An integer n is a power of four, if there exists an integer x such that n == 4x.
 
 class Solution(object):
     def isPowerOfFour(self, n):
